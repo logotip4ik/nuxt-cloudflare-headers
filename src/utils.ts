76d11e0d05f4b35d35b61bbf6685412b92579dcb
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-07-09 14:26:06 +0300

    chore: added utils functions and constants

---
+++ src/utils.ts
@@ -0,0 +1,24 @@
+import { useLogger } from "@nuxt/kit";
+
+import { ModuleOptions } from "./types";
+
+export const logger = useLogger("cloudflare-headers");
+
+export const headersStorageKey = "cloudflare:headers";
+
+export function stringify(obj: ModuleOptions) {
+  let result = "";
+
+  for (const [host, headers] of Object.entries(obj)) {
+    if (!result) result += host;
+    else result += `\n${host}`;
+
+    result += headers.reduce((acc, header) => {
+      const [headerName, headerValue] = Object.entries(header)[0];
+
+      return acc + `\n\t${headerName}: ${headerValue}`;
+    }, "");
+  }
+
+  return result;
+}
