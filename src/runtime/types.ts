93a83befa860bc14cc858234b135491b694e0900
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-07-09 22:22:22 +0300

    chore: removed unused files

--- src/runtime/types.ts
+++
@@ -1,7 +0,0 @@
-export interface ModuleOptions {
-  /**
-   * Key will be route matcher and value is an array of rules
-   * @see https://developers.cloudflare.com/pages/platform/headers
-   */
-  [key: string]: { [key: string]: string }[];
-}
