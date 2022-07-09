a6c3ab2aa83d1511f33649fec52b5fec46096b33
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-07-09 15:34:28 +0300

    feat: generated new jest config

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+export default {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  collectCoverage: true,
+  extensionsToTreatAsEsm: [".ts"],
+};
