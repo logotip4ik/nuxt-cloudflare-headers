fbeae4032930e26220b8535f6737af128a814b7b
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-07-09 15:55:20 +0300

    refactor: mergin options

--- src/module.ts
+++ src/module.ts
@@ -4,6 +4,7 @@ import {
   resolveModule,
   useLogger,
 } from "@nuxt/kit";
+import defu from "defu";
 
 import { ModuleOptions } from "./types";
 import { headersStorageKey } from "./utils";
@@ -17,7 +18,8 @@ export default defineNuxtModule<ModuleOptions>({
     compatibility: { nuxt: "^3.0.0" },
   },
   setup(options = {}, nuxt) {
-    const headers = { ...nuxt.options.cloudflareHeaders, ...options };
+    // @ts-ignore
+    const headers = defu(nuxt.options.cloudflareHeaders, options);
 
     const { resolve } = createResolver(import.meta.url);
     const resolveRuntimeModule = (path: string) =>
