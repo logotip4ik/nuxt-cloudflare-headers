bcf7161fed35d69509bca5fabf080c4bca7e596f
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-07-09 22:19:25 +0300

    feat: added default option for module + transpilation of the runtime folder + utils and package alias + runtime to nitro externals

--- src/module.ts
+++ src/module.ts
@@ -4,11 +4,10 @@ import {
   resolveModule,
   useLogger,
 } from "@nuxt/kit";
-import { defu } from "defu";
+import defaultsDeep from "defaults-deep";
 
 import { name, version } from "../package.json";
-import { ModuleOptions } from "./runtime/types";
-import { headersStorageKey } from "./runtime/utils";
+import { ModuleOptions } from "./types";
 
 const logger = useLogger("cloudflare-headers");
 
@@ -19,17 +18,32 @@ export default defineNuxtModule<ModuleOptions>({
     configKey: "cloudflareHeaders",
     compatibility: { nuxt: "^3.0.0" },
   },
+  defaults: () => ({}),
   setup(options = {}, nuxt) {
-    // @ts-ignore
-    const headers = defu(nuxt.options.cloudflareHeaders || {}, options);
+    // @ts-expect-error
+    const headers = defaultsDeep(options, nuxt.options.cloudflareHeaders || {});
 
     const { resolve } = createResolver(import.meta.url);
     const resolveRuntimeModule = (path: string) =>
       resolveModule(path, { paths: resolve("./runtime") });
 
-    nuxt.options.runtimeConfig[headersStorageKey] = headers;
+    // I found that this is probably the only method to share data between module and server route
+    nuxt.options.runtimeConfig[name] = headers;
+
+    nuxt.options.build.transpile = nuxt.options.build.transpile || [];
+    nuxt.options.build.transpile.push(resolve("./runtime"));
+
+    // This enables to import `stringify` function from `#utils` and `name` from `package.json`
+    nuxt.options.alias = nuxt.options.alias || {};
+    nuxt.options.alias["#utils"] = resolve("./runtime/utils");
+    nuxt.options.alias["#package"] = resolve("../package");
 
     nuxt.hook("nitro:config", (nitroConfig) => {
+      // Adding utils as external to nitro, so it will know where to import
+      nitroConfig.externals = nitroConfig.externals || {};
+      nitroConfig.externals.inline = nitroConfig.externals.inline || [];
+      nitroConfig.externals.inline.push(resolve("./runtime/utils"));
+
       nitroConfig.prerender = nitroConfig.prerender || {};
       nitroConfig.prerender.routes = nitroConfig.prerender.routes || [];
       nitroConfig.handlers = nitroConfig.handlers || [];
