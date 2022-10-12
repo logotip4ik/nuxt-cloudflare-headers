2962780c07113486400797b03f2e0acf3340b962
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-10-12 15:55:59 +0300

    feat: add better types

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,20 @@
+export interface Header {
+  [key: string]: string | false;
+}
+
 export interface ModuleOptions {
   /**
    * Key will be route matcher and value is an array of rules
    * @see https://developers.cloudflare.com/pages/platform/headers
+   *
+   * @example
+   * {
+   *   '/': [{ generatedBy: 'nuxtCloudflareHeaders' }],
+   *   '/admin': [{ generatedBy: false }]
+   * }
    */
-  [key: string]: { [key: string]: string | boolean }[];
+  [key: string]: Header[];
+
+  "/"?: Header[];
+  "/*"?: Header[];
 }
